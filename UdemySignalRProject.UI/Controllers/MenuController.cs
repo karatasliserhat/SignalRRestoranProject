5a1c695135e75b8dc3a90e23f69939f403755b59
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-02-21 13:54:53 +0300

    Menü controller yazıldı ürün id si ajax ile çekildi ekranda gösterildi

---
+++ UdemySignalRProject.UI/Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemySignalRProject.UI.IApiServices;
+
+namespace UdemySignalRProject.UI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IProductApiService _productApiService;
+
+        public MenuController(IProductApiService productApiService)
+        {
+            _productApiService = productApiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productApiService.GetListAsync("Product"));
+        }
+    }
+}
